48c0a556745081e2cd8fab8a08495fc704739e5e
Ajhar146 <ajhar.cse.146@gmail.com>
2025-12-31 19:10:10 +0600

    completed by_Ajhar_Dahy (process.h and process.c)

---
+++ process.h
@@ -0,0 +1,34 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define MAX_PROCESS 16
+
+typedef enum {
+    CURRENT,
+    READY,
+    TERMINATED
+} process_state_t;
+
+typedef struct context {
+    uintptr_t sp;
+    uintptr_t pc;
+} context_t;
+
+typedef struct pcb {
+    int pid;
+    process_state_t state;
+    void* stack;
+    context_t context;
+    struct pcb* next;
+} pcb_t;
+
+void process_init();
+int create_process(void (*entry)());
+void terminate_process(int pid);
+pcb_t* get_current_process();
+void set_current_process(pcb_t* p);
+
+#endif
